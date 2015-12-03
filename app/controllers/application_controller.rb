05140a013c213c23fc4f7fa30bbf37a0335b8f35
Piyush P Kurur <ppk@cse.iitk.ac.in>
2015-12-03 15:00:34 +0530

    add the common current_user and signed_in method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :warden, :current_user, :signed_in?
+
+
+  # Get the currently authorized user
+  def current_user
+    warden.user
+  end
+
+  # Authenticate.
+  def authenticate!
+    warden.authenticate!
+  end
+
+  # Get the warden date.
+  def warden
+    request.env['warden']
+  end
+
 end
