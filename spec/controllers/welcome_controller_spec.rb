a86365a2eb1da4f71efaae93f35873a3e29086c3
Piyush P Kurur <ppk@cse.iitk.ac.in>
2015-12-26 18:11:01 +0530

    fixed welcome controller.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
-    before do
-      get :index
-    end
+    context "Unauthenticate" do
+      before do
+        unauthenticated_access
+        get :index
+      end
 
-    subject { response }
-    it { is_expected.to have_http_status :ok    }
-    it { is_expected.to render_template(:index) }
+      subject { response }
+      it { is_expected.to have_http_status :ok    }
+      it { is_expected.to render_template(:index) }
+    end
+    context "Authenticated" do
+      let (:user) { FactoryGirl.create(:user) }
+      before do
+        authenticate_as user
+        get :index
+      end
+      it { is_expected.to redirect_to(user_path user.id) }
+    end
   end
 end
